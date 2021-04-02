066df23cdcc5e7601f45ace1741c04f150d2c008
pixegami <pixegami@gmail.com>
2021-04-02 21:44:05 +1100

    Spliting out core components

--- saas-site/src/components/auth/AuthRegister.tsx
+++ saas-site/src/components/auth/AuthRegister.tsx
@@ -1,8 +1,62 @@
 import { Link } from "gatsby";
 import * as React from "react";
+import { ReactNode } from "react";
 import AuthApi from "../../api/auth/AuthApi";
 import AuthResponse from "../../api/auth/AuthResponse";
 import { SubComponentBaseProps, withApiWrapper } from "./ApiComponentWrapper";
+import { ApiState } from "./ApiState";
+
+const createInputField = (
+  defaultValue: string,
+  setValue: (x: string) => void,
+  isDisabled: boolean
+) => {
+  return (
+    <input
+      className="border m-1 p-1 border-black"
+      type="text"
+      defaultValue={defaultValue}
+      onChange={(e) => setValue(e.currentTarget.value)}
+      disabled={isDisabled}
+    />
+  );
+};
+
+const createButton = (label: string, onClick: any, isDisabled: boolean) => {
+  return (
+    <button
+      className="border m-1 p-1 border-black"
+      type="button"
+      onClick={onClick}
+      disabled={isDisabled}
+    >
+      {label}
+    </button>
+  );
+};
+
+// Split the common form logic.
+
+// Split out the styling.
+
+const AuthCommonComponent: React.FC<{ apiState: ApiState }> = (props) => {
+  const busyElement = props.apiState.isBusy ? <div>Loading...</div> : null;
+  const errorElement = props.apiState.hasError ? (
+    <div className="text-red-500">Error: {props.apiState.errorMessage}</div>
+  ) : null;
+
+  return (
+    <>
+      <div className="text-lg">
+        <h1>This is the Auth Register Page.</h1>
+        {props.children}
+        {busyElement}
+        {errorElement}
+        <Link to="/app/landing">Back</Link>
+      </div>
+    </>
+  );
+};
 
 const AuthRegister: React.FC<SubComponentBaseProps> = (props) => {
   const [email, setEmail] = React.useState("");
@@ -19,43 +73,12 @@ const AuthRegister: React.FC<SubComponentBaseProps> = (props) => {
     AuthApi.delayedError().then(props.onApiResponse).catch(onCustomFault);
   };
 
-  const busyElement = props.apiState.isBusy ? <div>Loading...</div> : null;
-  const errorElement = props.apiState.hasError ? (
-    <div className="text-red-500">Error: {props.apiState.errorMessage}</div>
-  ) : null;
-
   return (
-    <div className="text-lg">
-      <h1>This is the Auth Register Page.</h1>
-
-      <input
-        className="border m-1 p-1 border-black"
-        type="text"
-        defaultValue={email}
-        onChange={(e) => setEmail(e.currentTarget.value)}
-        disabled={props.apiState.isBusy}
-      />
-      <input
-        className="border m-1 p-1 border-black"
-        type="password"
-        defaultValue={password}
-        onChange={(e) => setPassword(e.currentTarget.value)}
-        disabled={props.apiState.isBusy}
-      />
-      <button
-        className="border m-1 p-1 border-black"
-        type="button"
-        onClick={onRegister}
-        disabled={props.apiState.isBusy}
-      >
-        Register
-      </button>
-
-      {busyElement}
-      {errorElement}
-
-      <Link to="/app/landing">Back</Link>
-    </div>
+    <AuthCommonComponent apiState={props.apiState}>
+      {createInputField(email, setEmail, props.apiState.isBusy)}
+      {createInputField(password, setPassword, props.apiState.isBusy)}
+      {createButton("Register", onRegister, props.apiState.isBusy)}
+    </AuthCommonComponent>
   );
 };
 
