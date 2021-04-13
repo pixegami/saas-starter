61579685a56c8ca0f4cf39eae2adbaaf06846ddb
pixegami <pixegami@gmail.com>
2021-04-13 22:34:53 +1000

    Add VerifyAccount page

---
+++ saas-site/src/components/auth/AuthVerifyAccount.tsx
@@ -0,0 +1,83 @@
+import * as React from "react";
+import AuthApi from "../../api/auth/AuthApi";
+import { SubComponentBaseProps, withApiWrapper } from "./ApiComponentWrapper";
+import "../../styles/loader.css";
+import AuthCommonComponent from "./AuthCommonComponent";
+import ApiButton from "./ApiButton";
+import ApiStringField from "./ApiStringField";
+import ApiTextLink from "./ApiTextLink";
+import ApiResponse from "../../api/ApiResponse";
+import { navigate } from "gatsby";
+
+const AuthVerifyAccount: React.FC<SubComponentBaseProps> = (props) => {
+  const [verificationEmailSent, setVerificationEmailSent] = React.useState(
+    false
+  );
+
+  const onSuccess = (response: ApiResponse) => {
+    setVerificationEmailSent(true);
+    props.onApiResponse(response);
+  };
+
+  const onClick = () => {
+    props.onApiRequest();
+    AuthApi.delayedSuccess().then(onSuccess).catch(props.onApiFault);
+  };
+
+  const goToSignIn = () => {
+    navigate("/app/signIn");
+  };
+
+  const resendInstructions: string = "Didn't receive it?";
+
+  const isDisabled: boolean = props.apiState.isBusy || verificationEmailSent;
+  let interactiveElement = null;
+
+  if (!verificationEmailSent) {
+    const actionButton = (
+      <ApiButton
+        label="Re-send Verification Email"
+        onClick={onClick}
+        isDisabled={isDisabled}
+        isLoading={isDisabled}
+      />
+    );
+    interactiveElement = (
+      <div>
+        <div className="mt-4 text-gray-400 -mb-2">{resendInstructions}</div>
+        {actionButton}
+      </div>
+    );
+  } else {
+    const successPrompt = (
+      <div className="bg-blue-50 border-blue-600 border text-blue-600 p-4 py-6 rounded-md mb-6">
+        <div className="text-sm">
+          Verification email has been sent! Please check your inbox.
+        </div>
+      </div>
+    );
+    interactiveElement = <div>{successPrompt}</div>;
+  }
+
+  return (
+    <AuthCommonComponent
+      header="Verify your account."
+      apiState={props.apiState}
+    >
+      <div className="md:text-base text-sm text-gray-800 mb-6">
+        <div className="font-semibold mb-2">
+          You must verify your account before you can use it.
+        </div>
+        <div className="text-gray-600">
+          An email has been sent to X with instructions on how to do this.
+        </div>
+      </div>
+      {interactiveElement}
+      <div className="mt-4">
+        <ApiTextLink linkPath="/app/signIn" linkText="Back to Sign In" />
+      </div>
+    </AuthCommonComponent>
+  );
+};
+
+export const WrappedAuthVerifyAccount = withApiWrapper(AuthVerifyAccount);
--- saas-site/src/pages/app.tsx
+++ saas-site/src/pages/app.tsx
@@ -10,11 +10,16 @@ import { WrappedAuthSignIn } from "../components/auth/AuthSignIn";
 import { WrappedApiSubComponent } from "../components/auth/ApiSubComponent";
 import { WrappedAuthForgotPassword } from "../components/auth/AuthForgotPassword";
 import { WrappedAuthResetPassword } from "../components/auth/AuthResetPassword";
+import { WrappedAuthVerifyAccount } from "../components/auth/AuthVerifyAccount";
 
 const App = () => (
   <Router>
     <AuthRoute component={Landing} path="/app" bypassAuth />
     <AuthRoute component={Dashboard} path="/app/dashboard" />
+    <AuthRoute
+      component={WrappedAuthVerifyAccount}
+      path="/app/verify_account"
+    />
     <WrappedApiSubComponent path="/app/sc" />
     <WrappedAuthRegister path="/app/register" />
     <WrappedAuthSignIn path="/app/signIn" />
