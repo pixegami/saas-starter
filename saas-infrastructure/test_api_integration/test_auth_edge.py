990f74c8947b7b0873b105ceff760d9927233f34
pixegami <pixegami@gmail.com>
2021-04-24 21:07:30 +1000

    Add test for case-insensitive account reset

--- saas-infrastructure/test_api_integration/test_auth_edge.py
+++ saas-infrastructure/test_api_integration/test_auth_edge.py
@@ -5,11 +5,18 @@ from utils import *
 ############################################
 
 
-def test_sign_up_diff_case():
-    # I can sign up with non-case-sensitive emails.
-    user = generate_random_email()
+def test_sign_diff_case():
+    # I can sign up and reset password with non-case-sensitive emails.
+    user = generate_random_email(VALIDATION_EMAIL)
     password = generate_random_password()
-    sign_up(user, password, 200)
+    sign_up_test_user(user, password, 200)
 
     # Sign-in should succeed.
     sign_in(user.upper(), password, 200)
+
+    # Request account reset with upper-case user name.
+    request_account_reset(user.upper(), 200)
+
+    # Give SES time to process the email.
+    time.sleep(5)
+    sign_in(user, AUTO_RESET_PASSWORD)
